1243c6770a23f05aac4ecc60d69375e497db0092
Paul Zijlmans <paulzijlmans@hotmail.com>
2022-11-08 15:35:46 +0100

    initial commit

---
+++ src/main/java/nl/paulzijlmans/axoncqrseventsourcing/AxonCqrsEventSourcingApplication.java
@@ -0,0 +1,13 @@
+package nl.paulzijlmans.axoncqrseventsourcing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AxonCqrsEventSourcingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AxonCqrsEventSourcingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/nl/paulzijlmans/axoncqrseventsourcing/AxonCqrsEventSourcingApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.paulzijlmans.axoncqrseventsourcing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AxonCqrsEventSourcingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
